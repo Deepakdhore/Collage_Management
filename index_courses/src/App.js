4e6781c00513fefc82874a5c0095bdaf6e1daa9c
Deepakdhore <87926172+Deepakdhore@users.noreply.github.com>
2024-10-15 19:30:38 +0530

    add tostify,axios and reactstrap libraries

--- index_courses/src/App.js
+++ index_courses/src/App.js
@@ -1,8 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
+//import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header"
+import {Button} from "reactstrap";
 
-function App() {
-  return <h1> this is my react js app</h1>;
-}
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App()
+{
+
+const notify = () => toast("Wow so easy!");
+
+ return(
+        <div >
+           <h1>Header</h1>
+            <p>this is teh main app.js block</p>
+            <Header name="ramesh"/>
+
+            <Button color='warning' onClick={notify}> First react button </Button>
+            <button onClick={notify}>Notify!</button>
+            <ToastContainer />
+            <p>this is teh main app.js block</p>
+            <Header name="suresh"/>
+
+        </div>
+    );
+ }
 
 export default App;
---
+++ index_courses/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header({ name })
+{
+    return(
+        <div style={{background:'yellow',padding:20}}>
+            <h1>Header</h1>
+            <h1>learn code with {name}</h1>>
+            <h1>learn code with durgesh</h1>
+        </div>
+    );
+}
+
+export default Header;
