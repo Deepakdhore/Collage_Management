405c857dea7876ccfa4c8d6b3d131d9558b37f3f
Deepakdhore <87926172+Deepakdhore@users.noreply.github.com>
2024-10-16 21:09:48 +0530

    adding new header and home js file

--- index_courses/src/App.js
+++ index_courses/src/App.js
@@ -1,29 +1,22 @@
 import React from "react";
 //import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header"
+import Header from "./components/Header";
 import {Button} from "reactstrap";
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Home from "./components/Home";
 function App()
 {
 
 const notify = () => toast("Wow so easy!");
 
  return(
-        <div >
-           <h1>Header</h1>
-            <p>this is teh main app.js block</p>
-            <Header name="ramesh"/>
-
-            <Button color='warning' onClick={notify}> First react button </Button>
-            <button onClick={notify}>Notify!</button>
-            <ToastContainer />
-            <p>this is teh main app.js block</p>
-            <Header name="suresh"/>
-
+        <div>
+           //<h1>Welcome to courses application</h1>
+        <Home/>
         </div>
     );
  }
---
+++ index_courses/src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Card, CardBody, CardTitle, CardText } from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {Button} from "reactstrap";
+
+
+const Home = () => {
+
+const notify = () => toast("Wow so easy!");
+    return (
+
+       <div className="custom-jumbotron">
+
+                   <Container className="display-3 text-center">
+                       <h1>Learn Code with Durgesh</h1>
+                       <p>This is a paragraph. Please handle carefully; first, try to consider it as an example.</p>
+                        <Button color="primary"onClick={notify}>Notify!</Button>
+                               <ToastContainer />
+
+                   </Container>
+       </div>
+    );
+};
+
+export default Home;
