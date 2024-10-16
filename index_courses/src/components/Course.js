66f908af7db7b7889c659c21b02bbbc52bc7298e
Deepakdhore <87926172+Deepakdhore@users.noreply.github.com>
2024-10-16 21:17:21 +0530

    adding new course file

--- index_courses/src/components/Course.js
+++ index_courses/src/components/Course.js
@@ -1,1 +1,25 @@
-import React
+import React from "react";
+import{
+    Card,
+    Cardbody,
+    CardTitle,
+    CardSubtitile,
+    CardText,
+    CardFooter,
+    Button,
+    Container,
+} from "reactstrap";
+
+const Course=()=>{
+    return(
+        <Card>
+            <Cardbody>
+                <CardSubtitile>Java Course</CardSubtitile>
+                <CardText>It is a java course for beginers</CardText>
+                    <Container>
+                        <Button>Delete</Button>
+                    </Container>
+            </Cardbody>
+        </Card>
+    )
+}
